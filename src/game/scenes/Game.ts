4d5be47c96a74df4dab0bfd81b693db967f5b972
DimitarYosifov <yosifov1985@gmail.com>
2025-07-05 22:06:05 +0300

    basic move and attack for opponent done

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -44,7 +44,6 @@ export class Game extends Scene {
 
         this.events.on('direction-selected', (data: number[]) => {
 
-            // this.currentlySelectedMonster.pendingAction = false;
             const newRow = data[0];
             const newCol = data[1];
 
@@ -65,13 +64,13 @@ export class Game extends Scene {
             }
         });
 
+
+
         this.events.on('target-selected', (data: number[]) => {
 
             const newRow = data[0];
             const newCol = data[1];
             const isRanged = data[2];
-
-            // const currentData = this.currentlySelectedMonster.unitData;
 
             this.movementArrowsContainer.removeArrows();
             this.currentlySelectedMonster.performHit(newRow, newCol);
@@ -79,7 +78,8 @@ export class Game extends Scene {
             const isPlayerTurn = this.data.list.isPlayerTurn;
             const damage = isRanged ? this.currentlySelectedMonster.unitData.ranged : this.currentlySelectedMonster.unitData.melee;
             if (isPlayerTurn) {
-                const target: Monster = this.data.list.opponentMonsters.find((m: Monster) => m.unitData.row === newRow && m.unitData.col === newCol);
+                console.log(this.data.list.opponentMonsters)
+                const target: Monster = this.data.list.opponentMonsters.find((m: Monster) => m && m.unitData.row === newRow && m.unitData.col === newCol);
                 // target.setScale(2) // test
                 target.takeDamege(damage);
             } else {
@@ -112,7 +112,6 @@ export class Game extends Scene {
         if (turnEnd) {
             alert('end turn');
             this.data.list.isPlayerTurn = !this.data.list.isPlayerTurn;
-
             if (this.data.list.isPlayerTurn) {
                 this.addInteraction();
             } else {
@@ -218,7 +217,6 @@ export class Game extends Scene {
 
         this.data.list.opponentMonsters.forEach((monster: Monster) => {
             if (monster) {
-                // monster.setInteraction(!this.data.list.isPlayerTurn);  FOR TESTING!!!
                 monster.pendingAction = !this.data.list.isPlayerTurn;
             }
         });
@@ -253,36 +251,31 @@ export class Game extends Scene {
     }
 
     checkMapVisibility(showImediatelly: boolean = false) {
-        // if (this.data.list.isPlayerTurn) {  //P L A Y E R   M O V E
-            const playerMonsters = this.data.list.playerMonsters.filter((m: Monster | null) => m !== null);
-            this.data.list.clouds.forEach((row: Cloud[]) => {
-                row.forEach((cloud: Cloud) => {
-                    if (showImediatelly) {
-                        cloud.setAlpha(0.7);
-                    } else {
-                        cloud.toggleVisibility(0.7);
-                    }
-                });
+        //P L A Y E R   M O V E
+        const playerMonsters = this.data.list.playerMonsters.filter((m: Monster | null) => m !== null);
+        this.data.list.clouds.forEach((row: Cloud[]) => {
+            row.forEach((cloud: Cloud) => {
+                if (showImediatelly) {
+                    cloud.setAlpha(1);
+                } else {
+                    cloud.toggleVisibility(1);
+                }
             });
-            playerMonsters.forEach((m: Monster) => {
-                const visibleCells = this.getVisibleCells(m.unitData.row, m.unitData.col, m.unitData.vision);
-                visibleCells.forEach((cell: { row: number, col: number }) => {
-                    if (showImediatelly) {
-                        this.data.list.clouds[cell.row][cell.col].setAlpha(0);
-                    } else {
-                        this.data.list.clouds[cell.row][cell.col].toggleVisibility(0);
-                    }
-                });
+        });
+        playerMonsters.forEach((m: Monster) => {
+            const visibleCells = this.getVisibleCells(m.unitData.row, m.unitData.col, m.unitData.vision);
+            visibleCells.forEach((cell: { row: number, col: number }) => {
+                if (showImediatelly) {
+                    this.data.list.clouds[cell.row][cell.col].setAlpha(0);
+                } else {
+                    this.data.list.clouds[cell.row][cell.col].toggleVisibility(0);
+                }
             });
-        // }
-        // else { // O P P O N E N T   M O V E 
-        //     // alert(123)
-        //     // this.getRandomOpponentMonster();
-        // }
+        });
     }
 
     private getRandomOpponentMonster() {
-        const opponentMonsters = this.data.list.opponentMonsters.filter((m: Monster | null) => m!.pendingAction && m !== null);
+        const opponentMonsters = this.data.list.opponentMonsters.filter((m: Monster | null) => m !== null && m!.pendingAction);
         const rndMonsterIndex = Phaser.Math.RND.between(0, opponentMonsters.length - 1);
         this.currentlySelectedMonster = opponentMonsters[rndMonsterIndex];
         this.mainGridContainer.bringToTop(this.currentlySelectedMonster);
@@ -292,7 +285,7 @@ export class Game extends Scene {
 
         // get all positions visible to opponent
         const allVisibleCellsToOpponent = Array.from({ length: cols }, () => Array(rows).fill(false));
-        opponentMonsters.forEach((m: Monster) => {
+        this.data.list.opponentMonsters.filter((m: Monster | null) => m !== null).forEach((m: Monster) => {
             const visibleCells = this.getVisibleCells(m.unitData.row, m.unitData.col, m.unitData.vision);
             visibleCells.forEach((cell: { row: number, col: number }) => {
                 allVisibleCellsToOpponent[cell.row][cell.col] = true;
@@ -305,36 +298,245 @@ export class Game extends Scene {
             this.currentlySelectedMonster.unitData.col,
             range
         );
+
+        // all player monsters in range by current opponent's monster
         visibleTargetsToCurrentOpponentMonster = visibleTargetsToCurrentOpponentMonster.filter(x => x.occupiedBy === 'player');
-
-
-
-
         if (visibleTargetsToCurrentOpponentMonster.length > 0) {
             //ATTACK
             const targetForOpponentCurrentMonster = this.getRandomTargetForOpponent(visibleTargetsToCurrentOpponentMonster);
             console.log(visibleTargetsToCurrentOpponentMonster)
+            console.log(targetForOpponentCurrentMonster)
             const targetData = targetForOpponentCurrentMonster;
             this.events.emit('target-selected', [targetData.row, targetData.col, this.currentlySelectedMonster.unitData.ranged > 0])
-        } else if (true) {
-            // JUST MOVE FORWARD
-            this.events.emit('direction-selected', [this.currentlySelectedMonster.unitData.row, this.currentlySelectedMonster.unitData.col + 1,])
-            //TODO  - ELSE IF  knows position of player monster - FIND CLOSEST
-        }
-        else {     //TODO -  random move - most likely forward
+        } else {
+            if (!this.checkPossibleMove()) {
+                this.currentlySelectedMonster.skipMove();
+                return;
+            }
+            const closestMonster = this.findClosestPlayerMonsterToMoveTo(allVisibleCellsToOpponent);
+            if (closestMonster) {
+                //MOVE TOWARDS CLOSEST PLAYER'S MONSTER VISIBLE BY ANY OPPONENT'S MONSTER 
+                const newPosition = this.getMove(closestMonster.unitData);
+                this.events.emit('direction-selected', [newPosition.row, newPosition.col]);
+            }
+            else {
+                // MOVE TO RANDOM POSITION
+                const newPosition = this.getRandomDirection();
+                this.events.emit('direction-selected', [newPosition!.newRow, newPosition!.newCol]);
+            }
 
         }
-        //TODO - move to separate class!
     }
 
-    private getRandomTargetForOpponent(visibleTargetsToCurrentOpponentMonster: any): any {
-        return visibleTargetsToCurrentOpponentMonster[Phaser.Math.RND.between(0, visibleTargetsToCurrentOpponentMonster.length - 1)];
+    private getRandomTargetForOpponent(targets: any): any {
+        return targets[Phaser.Math.RND.between(0, targets.length - 1)];
     }
 
+    private findClosestPlayerMonsterToMoveTo(allVisibleCellsToOpponent: any[][]) {
+        const x1 = this.currentlySelectedMonster.unitData.col;
+        const y1 = this.currentlySelectedMonster.unitData.row;
+
+        let shortestDistance = Infinity;
+        let coords = {
+            x: -1,
+            y: -1
+        }
+
+        for (let row = 0; row < main_config.gridSizeVertical; row++) {
+            for (let col = 0; col < main_config.gridSizeHorizontal; col++) {
+                if (allVisibleCellsToOpponent[row][col] && this.data.list.gridPositions[row][col].occupiedBy === 'player') {
+                    const x2 = col;
+                    const y2 = row;
+                    const distance = Phaser.Math.Distance.Between(x1, y1, x2, y2);
+                    console.log(distance);
+                    if (distance < shortestDistance) {
+                        shortestDistance = distance;
+                        coords.x = col;
+                        coords.y = row;
+                    }
+                }
+            }
+        }
+
+        console.log(this.data.list.playerMonsters)
+        const targetPlayerMonster = this.data.list.playerMonsters.find((m: Monster) => m && m.unitData.row === coords.y && m.unitData.col === coords.x);
+        console.log('targetPlayerMonster', targetPlayerMonster)
+        return targetPlayerMonster;
+    }
+
+    private checkPossibleMove(): boolean {
+        let row, col;
+        const currentUnitData = this.currentlySelectedMonster.unitData;
+        //UP
+        row = currentUnitData.row - 1;
+        col = currentUnitData.col;
+        if (this.isCellEmpty(row, col)) return true;
+
+        //UP RIGHT
+        row = currentUnitData.row - 1;
+        col = currentUnitData.col + 1;
+        if (this.isCellEmpty(row, col)) return true;
+
+        //RIGHT
+        row = currentUnitData.row;
+        col = currentUnitData.col + 1;
+        if (this.isCellEmpty(row, col)) return true;
+
+        //RIGHT DOWN
+        row = currentUnitData.row + 1;
+        col = currentUnitData.col + 1;
+        if (this.isCellEmpty(row, col)) return true;
+
+        //DOWN
+        row = currentUnitData.row + 1;
+        col = currentUnitData.col;
+        if (this.isCellEmpty(row, col)) return true;
+
+        //LEFT DOWN
+        row = currentUnitData.row + 1;
+        col = currentUnitData.col - 1;
+        if (this.isCellEmpty(row, col)) return true;
+
+        //LEFT
+        row = currentUnitData.row;
+        col = currentUnitData.col - 1;
+        if (this.isCellEmpty(row, col)) return true;
+
+        //UP LEFT
+        row = currentUnitData.row - 1;
+        col = currentUnitData.col - 1;
+        if (this.isCellEmpty(row, col)) return true;
+        alert()
+        return false;
+    }
+
+    private getMove(closestUnitData: IUnitData): { row: number, col: number, weight: number } {
+        const currentUnitData = this.currentlySelectedMonster.unitData;
+
+        const addMove = (row: number, col: number) => {
+            if (this.isCellEmpty(row, col)) {
+                moves.push({
+                    row,
+                    col,
+                    weight: (+(Math.abs(row - closestUnitData.row) < Math.abs(currentUnitData.row - closestUnitData.row)) + +(Math.abs(col - closestUnitData.col) < Math.abs(currentUnitData.col - closestUnitData.col)))
+                })
+            }
+        }
+        const moves: { row: number, col: number, weight: number }[] = [];
+        let row, col;
+
+        //UP
+        row = currentUnitData.row - 1;
+        col = currentUnitData.col;
+        addMove(row, col);
+
+        //UP RIGHT
+        row = currentUnitData.row - 1;
+        col = currentUnitData.col + 1;
+        addMove(row, col);
+
+        //RIGHT
+        row = currentUnitData.row;
+        col = currentUnitData.col + 1;
+        addMove(row, col);
+
+        //RIGHT DOWN
+        row = currentUnitData.row + 1;
+        col = currentUnitData.col + 1;
+        addMove(row, col);
+
+        //DOWN
+        row = currentUnitData.row + 1;
+        col = currentUnitData.col;
+        addMove(row, col);
+
+        //LEFT DOWN
+        row = currentUnitData.row + 1;
+        col = currentUnitData.col - 1;
+        addMove(row, col);
+
+        //LEFT
+        row = currentUnitData.row;
+        col = currentUnitData.col - 1;
+        addMove(row, col);
+
+        //UP LEFT
+        row = currentUnitData.row - 1;
+        col = currentUnitData.col - 1;
+        addMove(row, col);
+
+        console.log(moves);
+        const maxWeight = Math.max(...moves.map((x) => x.weight));
+        const bestMoves = moves.filter(move => move.weight === maxWeight);
+        const move = Phaser.Math.RND.pick(bestMoves);
+        return move;
+    }
+
+    private getRandomDirection(): { newRow: number, newCol: number } | undefined {
+
+        let row = this.currentlySelectedMonster.unitData.row;
+        let col = this.currentlySelectedMonster.unitData.col;
+        let newRow = NaN, newCol = NaN;
+
+        const check = () => {
+            const rnd = Phaser.Math.RND.between(1, 100);
+            //RIGHT - 20%
+            if (rnd > 80) {
+                newRow = row;
+                newCol = col + 1;
+            }
+            //UP RIGHT - 20%
+            else if (rnd > 60) {
+                newRow = row - 1;
+                newCol = col + 1;
+            }
+            //DOWN RIGHT - 20%
+            else if (rnd > 40) {
+                newRow = row + 1;
+                newCol = col + 1;
+            }
+            //UP - 15%
+            else if (rnd > 25) {
+                newRow = row - 1;
+                newCol = col;
+            }
+            //DOWN - 15%
+            else if (rnd > 10) {
+                newRow = row + 1;
+                newCol = col;
+            }
+            //DOWN LEFT - 3%
+            else if (rnd > 7) {
+                newRow = row + 1;
+                newCol = col - 1;
+            }
+            //LEFT - 3%
+            else if (rnd > 4) {
+                newRow = row;
+                newCol = col - 1;
+            }
+            //UP LEFT - 4%
+            else {
+                newRow = row - 1;
+                newCol = col - 1;
+            }
+            if (!this.isCellEmpty(newRow, newCol)) {
+                check();
+            }
+        }
+        check();
+
+
+
+        return { newRow, newCol }
+    }
+
+    private isCellEmpty(row: number, col: number): boolean {
+        return this.data.list.gridPositions[row] !== undefined &&
+            this.data.list.gridPositions[row][col] !== undefined &&
+            this.data.list.gridPositions[row][col].isEmpty;
+    }
 }
-
-
-
 
 export interface IGridDimensions {
     gridSizeHorizontal: number;
--- src/game/scenes/in-game/TestOpponentTeam.ts
+++ src/game/scenes/in-game/TestOpponentTeam.ts
@@ -8,14 +8,23 @@ export class TestOpponentTeam {
     static get team() {
         return [
             {
-                col: 0, row: 3, melee: 2, ranged: 0, health: 5, shield: 2, vision: 2, stars: 1, type: '8'
+                col: 1, row: 3, melee: 2, ranged: 0, health: 5, shield: 2, vision: 2, stars: 1, type: '5'
             },
             {
-                col: 1, row: 6, melee: 6, ranged: 0, health: 2, shield: 2, vision: 2, stars: 5, type: '8'
+                col: 1, row: 4, melee: 3, ranged: 0, health: 2, shield: 2, vision: 2, stars: 5, type: '7'
             },
-            // {
-            //     col: 3, row: 5, melee: 3, ranged: 0, health: 3, shield: 1, vision: 1, stars: 2, type: '5'
-            // }
+            {
+                col: 0, row: 0, melee: 4, ranged: 0, health: 3, shield: 1, vision: 1, stars: 2, type: '5'
+            },
+            {
+                col: 0, row: 3, melee: 2, ranged: 0, health: 7, shield: 2, vision: 2, stars: 1, type: '8'
+            },
+            {
+                col: 1, row: 4, melee: 4, ranged: 0, health: 2, shield: 2, vision: 1, stars: 5, type: '8'
+            },
+            {
+                col: 0, row: 6, melee: 2, ranged: 0, health: 4, shield: 1, vision: 1, stars: 2, type: '7'
+            }
         ]
     }
 
