172adc0ecc335cb7c4fe2ef2341ea29d22243e3e
DimitarYosifov <yosifov1985@gmail.com>
2026-01-01 22:51:22 +0200

    test scaling444

--- src/game/scenes/Boot.ts
+++ src/game/scenes/Boot.ts
@@ -1,22 +1,38 @@
 import { Scene } from 'phaser';
 
-export class Boot extends Scene
-{
-    constructor ()
-    {
+export class Boot extends Scene {
+    constructor() {
         super('Boot');
     }
 
-    preload ()
-    {
+    preload() {
         //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
         //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
 
         this.load.image('background', 'assets/bg3.jpg');
     }
 
-    create ()
-    {
+    resizeGame() {
+        const vv = window.visualViewport;
+
+        const width = Math.floor(vv ? vv.width : window.innerWidth);
+        const height = Math.floor(vv ? vv.height : window.innerHeight);
+
+        this.game.scale.resize(width, height);
+    }
+
+    create() {
+
+        window.addEventListener('resize', this.resizeGame);
+        window.addEventListener('orientationchange', this.resizeGame);
+
+        // visualViewport fires when address bar collapses
+        if (window.visualViewport) {
+            window.visualViewport.addEventListener('resize', this.resizeGame);
+            window.visualViewport.addEventListener('scroll', this.resizeGame);
+        }
+
+
         this.scene.start('Preloader');
     }
 }
