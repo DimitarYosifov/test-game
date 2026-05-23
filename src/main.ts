f8ed70c30362f1a8b35440161bb7560f63a81abe
DimitarYosifov <yosifov1985@gmail.com>
2026-05-23 22:32:42 +0300

    detection of another tab with game running in it changed

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,42 @@ import { createDebugPanel } from './game/debug-panel';
 import { LOCAL_STORAGE_MANAGER } from './game/LOCAL_STORAGE_MANAGER';
 import StartGame from './game/main';
 
-const gameOpen = localStorage.getItem('gameOpen');
-if (gameOpen === 'false' || gameOpen === null) {
-    document.addEventListener('DOMContentLoaded', () => {
-        LOCAL_STORAGE_MANAGER.ensureData();
-        console.log(`gameOpen => ${gameOpen}`);
-        StartGame('game-container');
-        localStorage.setItem('gameOpen', 'true');
+const channel = new BroadcastChannel('my_game');
+let hasOtherTab = false;
 
+channel.onmessage = (event) => {
+
+    if (event.data === 'GAME_OPEN') {
+        hasOtherTab = true;
+    }
+
+    if (event.data === 'PING') {
+        channel.postMessage('GAME_OPEN');
+    }
+};
+
+channel.postMessage('PING');
+
+setTimeout(() => {
+
+    if (hasOtherTab) {
+
+        document.body.innerHTML = `
+            <h1>Game already open</h1>
+        `;
+
+    } else {
+
+        StartGame('game-container');
+        LOCAL_STORAGE_MANAGER.ensureData();
+       
         if (main_config.allowDebugPanel) {
             createDebugPanel();
         }
-    });
-    window.addEventListener("pagehide", () => {
-        localStorage.setItem('gameOpen', 'false');
-    });
-}
+
+        window.addEventListener('beforeunload', () => {
+            channel.close();
+        });
+    }
+
+}, 300);
